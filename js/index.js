abc085659717799b1a674f3d25ed9a26224961ea
Donik <126549201+donikabdusalimov@users.noreply.github.com>
2023-03-31 15:06:27 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,40 @@
+// let students = [1, 2, 3, 'Farrux', true, 'hello ', ['guys', 'my', 11], true, 'name is ', [[0, 'years old', 0, 'and I am ']]]
+// console.log(students[5], students[6][0], students[6][1], students[8], students[3], students[9][0][3], students[6][2], students[9][0][1]);
+
+
+
+// let klient = +prompt('Сколько вам лет?')
+// if (klient >= 18) {
+//     console.log('welcome');
+// } else {
+//     console.log('goodby');
+// }
+
+
+
+// let name = prompt('Как вас зовут?').toLowerCase().trim()
+// let old = +prompt('Сколько вам лет?').trim()
+// if (name === 'alex' && old >= 18) {
+//     alert('Добропожаловать')
+// } else {
+//     alert('Досвидули')
+// }
+
+
+
+// let a = +prompt('Введите первое число')
+// let b = prompt('Введите метод')
+// let c = +prompt('Введите второе число')
+// if (b === '+') {
+//     alert(a + c)
+// }else if (b === '-'){
+//     alert(a-c)
+// }else if(b === '*'){
+//     alert(a*c)
+// }else if (b === '/'){
+//     alert(a/c)
+// }else{
+//     alert('Не правельный метод!')
+// }
+
+let a = +prompt('Введите число которую вы хотите проверять на чотность')
